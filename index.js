1f6367fcb80bf70bca0d0865a02043fa58fe72c0
raSel <mdraselm413@gmail.com>
2022-11-10 18:53:50 +0600

    page

--- index.js
+++ index.js
@@ -23,11 +23,11 @@ async function run(){
 
         app.get('/products', async(req, res) =>{
             const page = req.query.page;
-            const size = req.query.size;
+            const size = parseInt(req.query.size);
             console.log(page, size);
             const query = {}
             const cursor = productCollection.find(query);
-            const products = await cursor.toArray();
+            const products = await cursor.skip(page*size).limit(size).toArray();
             const count = await productCollection.estimatedDocumentCount()
             res.send({count,products})
         })
